19b3af85de3a0e366459e5dd287f5e6a30daaf0c
Avesh <116141506+Avesh-code@users.noreply.github.com>
2025-11-25 10:45:53 +0530

    Update build-docker.groovy

--- vars/build-docker.groovy
+++ vars/build-docker.groovy
@@ -1,6 +1,3 @@
 def call(string projectname, string imagetag){
-  withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
-      {  
-        sh "sh "sudo docker build -t "${env.dockerHubUser}"/"${projectname}":"${imagetag}" ."
-      }
+  sh "sudo docker build -t "${projectname}":"${imagetag}" .
 }
