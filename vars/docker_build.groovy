78ffba1ba6fb6bef0ea36dd1f94ee398984ecdc3
Avesh <116141506+Avesh-code@users.noreply.github.com>
2025-11-26 17:14:57 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String projectname, String imagetag){
-  sh """docker buildx build -t "${projectname}:${imagetag}" ."""
+  sh """docker buildx build --tag "${projectname}:${imagetag}"  ."""
 }
