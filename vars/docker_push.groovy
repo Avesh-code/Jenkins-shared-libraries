97eedf1c5bbf9492c5e9ef6394ef541b0cf538cb
Avesh <116141506+Avesh-code@users.noreply.github.com>
2025-11-26 16:40:32 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String projectname,String imagename){
   withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")])
   {
-    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass} "
+    sh """docker login -u "${env.dockerHubUser}" -p "${env.dockerHubPass}" """
     sh "docker image tag ${projectname}:${imagename} ${env.dockerHubUser}/${projectname}:${imagename} "
     sh "docker push ${env.dockerHubUser}/${projectname}:${imagename} "
   }
